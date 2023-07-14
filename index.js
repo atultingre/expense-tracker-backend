570074db0f3e0061fc4c2704ced095c42da25302
ATUL TINGRE <atultingre.work@gmail.com>
2023-07-14 11:38:14 +0530

    expense-tracker-backend

---
+++ index.js
@@ -0,0 +1,177 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const BASE_URL = process.env.BASE_URL;
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+// Define Expense model
+const Expense = mongoose.model("Expense", {
+  title: String,
+  amount: Number,
+  date: Date,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+// Define User model
+const User = mongoose.model("User", {
+  username: String,
+  password: String,
+});
+
+// Parse request body as JSON
+app.use(express.json());
+
+// Middleware to verify the JWT token and extract the user ID
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+// Define API endpoints
+
+// Register user
+app.post(`/api/register`, async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+    // Generate a JWT token
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET);
+
+    res.json({ token });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Failed to register user" });
+  }
+});
+
+// Login user
+app.post(`/api/login`, async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    // Compare the provided password with the stored password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.json({ token });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: "Failed to login" });
+  }
+});
+
+// Apply the authentication middleware to the expense endpoints
+
+// GET THE EXPENSES
+app.get(`/api/expenses`, authenticateToken, async (req, res) => {
+  try {
+    const expenses = await Expense.find({ userId: req.user.userId });
+    res.json(expenses);
+  } catch (error) {
+    console.error("Error fetching expenses:", error);
+    res.status(500).json({ error: "Failed to fetch expenses" });
+  }
+});
+
+// POST THE EXPENSES
+app.post(`/api/expenses`, authenticateToken, async (req, res) => {
+  try {
+    const expense = new Expense({
+      ...req.body,
+      userId: req.user.userId,
+    });
+    await expense.save();
+    res.json(expense);
+  } catch (error) {
+    console.error("Error saving expense:", error);
+    res.status(500).json({ error: "Failed to save expense" });
+  }
+});
+
+// UPDATE THE EXPENSES
+app.put(`/api/expenses/:id`, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const expense = await Expense.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(expense);
+  } catch (error) {
+    console.error("Error updating expense:", error);
+    res.status(500).json({ error: "Failed to update expense" });
+  }
+});
+
+// DELETE THE EXPENSES
+app.delete(`/api/expenses/:id`, async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Expense.findByIdAndDelete(id);
+    res.sendStatus(204);
+  } catch (error) {
+    console.error("Error deleting expense:", error);
+    res.status(500).json({ error: "Failed to delete expense" });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
